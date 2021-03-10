cba37022d7722d5f8b9d7b09df1ef73fd52aa941
tangent <tangent10crypto@gmail.com>
2021-03-09 20:27:12 -0500

    Fix tsukibnbnCakeLP address

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,5 +1,5 @@
 export const tsuki = '0x3fd9e7041c45622e8026199a46f763c9807f66f3'
 export const bnbc  = '0x569fF542CCf66a1F15BA018802b6BDB6263C9b44'
-export const tsukibnbCakeLP = '0xfeD89dEBB8d6C9140360007960E0ecF329A18755'
+export const tsukibnbCakeLP = '0x060425E600893F24847c6eb6aD3d1Cd6faecb1Bc'
 export const bnbcbnbCakeLP = '0x558a6847be1256F9a7a0b215391a81fF68cf719B'
 
